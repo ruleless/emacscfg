d01f3810daeac2f158cd86b984f104bce3470ce2
ruleless <ruleless@126.com>
2015-07-09 06:49:08 +0800

    换个颜色主题（在putty下外观勉强凑合）

--- emacs.d/settings-color.el
+++ emacs.d/settings-color.el
@@ -2,6 +2,9 @@
 
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-dark-blue2)
+;; (color-theme-dark-blue2)
+;; (color-theme-gray1)
+(color-theme-charcoal-black)
+;; (color-theme-clarity)
 
 (provide 'settings-color)
