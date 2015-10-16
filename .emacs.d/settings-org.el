fcb5ebe47646123d589d9d1bd1f45a731bdc2b08
ruleless <ruleless@126.com>
2015-10-16 10:22:33 +0800

    Emacs Org(add some shortkeys)

--- .emacs.d/settings-org.el
+++ .emacs.d/settings-org.el
@@ -5,4 +5,10 @@
 
 (setq org-src-fontify-natively t)
 
+;; The following lines are always needed. Choose your own keys.
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (provide 'settings-org)
