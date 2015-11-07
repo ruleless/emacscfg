ff5a027c47381707e64176f134c19d5c6b2caaa2
ruleless <ruleless@126.com>
2015-11-07 20:34:00 +0800

    删除不必要的shell文件

--- python27-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /usr/src/Python-2.7.3/
-./configure --prefix=/usr/local/python2.7
-make all
-make install
-make clean
-make distclean
--- python27-download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ~/download/
-wget http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
